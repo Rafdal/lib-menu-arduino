91ca2737272cf587666b98295c885caa2a465faf
Rafael <rdalzotto@itba.edu.ar>
2022-09-29 16:40:11 -0300

    first commit

---
+++ Menu.cpp
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <assert.h>
+#include "Menu.h"
+
+static menu_callback_t event_listener = NULL; // Callback lector de nuevos eventos
+static menu_callback_t update_display = NULL; // Callback para actualizar datos de interfaz de usuario
+static menu_callback_t display_animations = NULL; // Callback ejecutada en tiempo real (OPCIONAL)
+
+static Menu *current_menu = NULL;    // Puntero al menu del contexto actual (en ejecucion)
+
+// Inicializar un menu
+Menu::Menu(uint8_t options, char* title, menu_callback_t on_exit, menu_action_t action)
+{
+    // Asignar memoria a los arreglos de informacion de las opciones
+    option_titles = (char**) calloc(options, sizeof(char*));
+    option_callbacks = (menu_callback_t*) calloc(options, sizeof(menu_callback_t));
+
+    // FIXME
+    // Si ocurre un error liberar todo (ESTO NO DEBERIA PASAR)
+    if(option_titles == NULL || option_callbacks == NULL){
+        free(option_titles);
+        free(option_callbacks);
+    }else{
+        // Asignar los datos
+        title = title;
+        current_option = 0;
+        n_options = options;
+        on_exit = on_exit;
+        exit_action = action;
+    }
+}
+
+Menu::~Menu()
+{
+    free(option_titles);
+    free(option_callbacks);
+}
+
+void Menu::set_option(uint8_t option_id, char* title, menu_callback_t callback){
+    option_titles[option_id] = title;
+    option_callbacks[option_id] = callback;
+}
+
+// ABRIR Y EJECUTAR UN MENU
+void Menu::run(){
+    // Sin los callbacks event_listener y update_display, el menu no funciona!
+    if(event_listener != NULL && update_display != NULL){
+
+        state = MENU_STATE_AVAILABLE; // Activar el menu
+        current_option=0;             // Focusear la primera opcion por defecto
+
+        current_menu = this;                // Setear el menu como menu actual (en foco)
+        update_display();                   // Renderizar el menu
+
+        while (state != MENU_STATE_CLOSE) // Mientras el menu no se haya cerrado o este cerrado
+        {        
+            event_listener();                   // escuchar cambios de estado (bajar,seleccionar,cerrar,etc)
+
+            switch (state)
+            {
+            case MENU_STATE_AVAILABLE:
+                /* do nothing */
+                break;
+
+            case MENU_STATE_DOWN: // Ir a la opcion inferior
+                if(current_option < n_options-1)
+                    current_option++;
+                break;
+
+            case MENU_STATE_UP: // Ir a la opcion superior
+                if(current_option >0)
+                    current_option--;
+                break;
+
+            case MENU_STATE_SELECT: // Seleccionar la opcion actual
+                if(option_callbacks[current_option] != NULL){
+                    option_callbacks[current_option]();
+                    current_menu = this; // retomo el control (en caso de que se haya perdido)
+                }
+                else{
+                    // printf("null Callback!\n");
+                }
+                break;
+
+            case MENU_STATE_BACK:
+                switch (exit_action)
+                {
+                case MENU_ACTION_CALLBACK:
+                    if(on_exit != NULL)
+                        on_exit();                 // ejecuto la funcion de salida
+                    break;
+                
+                case MENU_ACTION_DO_NOTHING:
+                    break; // do nothing
+
+                case MENU_ACTION_JUST_EXIT:
+                default:
+                    state = MENU_STATE_CLOSE;    // cierro el menu (por defecto)
+                    break;
+                }
+                break;
+
+            default:
+                // printf("Menu Event Error\n");
+                break;
+            }
+
+            if(display_animations != NULL)
+                display_animations(); // Si hay animaciones, ejecutar
+
+            // Si estamos en un estado de transicion, actualizamos el display
+            if(state != MENU_STATE_AVAILABLE && state != MENU_STATE_CLOSE){
+                state = MENU_STATE_AVAILABLE;
+                update_display();
+            }
+        }
+        // Serial.println(F("saliendo del menu\n"));
+    }
+}
+
+MenuData Menu::getData()
+{
+    MenuData data;
+    data.current_option = current_option;
+    data.n_options = n_options;
+    data.option_titles = option_titles;
+    data.title = title;
+    return data;
+}
+
+bool Menu::is_available(){
+    return (state == MENU_STATE_AVAILABLE); // En los estados intermedios (SELECT, UP, etc) se considera que el menu esta ocupado
+}
+
+void Menu::go_up(void){
+    state = MENU_STATE_UP;
+}
+
+void Menu::go_down(void){
+    state = MENU_STATE_DOWN;
+}
+
+void Menu::go_select(void){
+    state = MENU_STATE_SELECT;
+}
+
+void Menu::go_back(void){
+    state = MENU_STATE_BACK;
+}
+
+void Menu::force_close(){
+    state = MENU_STATE_CLOSE;
+}
+
+
+// OPCIONAL. Loop en tiempo real para animaciones (usado en raspberry pi)
+void menu_set_animation_callback(menu_callback_t animation_f){
+    display_animations = animation_f;
+}
+
+void menu_set_event_listener_display(menu_callback_t ev_listener, menu_callback_t _update_display){
+    event_listener = ev_listener;
+    update_display = _update_display;
+}
+
+
+void menu_go_up(void){
+    current_menu->go_up();
+}
+
+void menu_go_down(void){
+    current_menu->go_down();
+}
+
+void menu_go_select(void){
+    current_menu->go_select();
+}
+
+void menu_go_back(void){
+    current_menu->go_back();
+}
+
+MenuData menu_get_current_data(void){
+    if(current_menu != NULL){
+        return current_menu->getData();
+    }
+    return MenuData();
+}
+
+bool menu_is_current_available(void){
+    return current_menu->is_available();
+}
+
+void menu_force_close_current(void){
+    current_menu->force_close();
+}
---
+++ Menu.h
@@ -0,0 +1,139 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include <Arduino.h>
+
+/********************************************************************************
+ *                              LIBERIA MENU                                    *
+ * Esta libreria permite crear diferentes menues y poder controlarlos           *
+ * facilmente, navegar entre sus opciones, crear sub-menues y estructuras       *
+ * complejas de menues dentro de otros menues con total libertad                *
+ *                                                                              *
+ * Tambien permite acceder con facilidad al los datos del menu actual para poder*
+ * mostrarlos por pantalla (lista de opciones, opcion seleccionada, titulo, etc)*
+ *                                                                              *
+ * Esta libreria NO cumple funciones de frontend, el programador decide como    *
+ * conectar esta libreria con el frontend y que interfaz usara                  *
+ ********************************************************************************
+ */
+
+// Acciones del menu al ejecutar menu_go_back()
+typedef enum {
+    MENU_ACTION_DO_NOTHING, // No hacer nada al ir hacia atras (no salir)
+    MENU_ACTION_JUST_EXIT,  // Salir del menu al ir hacia atras
+    MENU_ACTION_CALLBACK,   // Ejecutar un callback al ir hacia atras
+} menu_action_t;
+
+// Estados del menu
+typedef enum {
+    MENU_STATE_CLOSE,       // Cerrar menu | menu cerrado
+    MENU_STATE_AVAILABLE,   // Menu activo en espera
+    MENU_STATE_UP,      // Ir a la opcion superior (estado de transicion)
+    MENU_STATE_DOWN,    // Ir a la opcion inferior (estado de transicion)
+    MENU_STATE_BACK,    // Ir hacia atras (estado de transicion)
+    MENU_STATE_SELECT   // Seleccionar opcion (estado de transicion)
+} menu_state_t;
+
+typedef void (*menu_callback_t)(void); // Void Callback
+
+typedef struct MenuData
+{
+    MenuData() { n_options = 0; title = NULL; }
+    char **option_titles;   // titulos de cada opcion    
+    char *title;            // Titulo del menu
+    uint8_t n_options;      // cantidad de opciones
+    uint8_t current_option; // opcion actual
+};
+
+class Menu
+{
+    
+public:
+    /**
+     * @brief Inicializar menu. Se ejecuta una sola vez al principio del programa, en la configuracion
+     * 
+     * - EJEMPLO: menu_t *cfg_menu = menu_init(3, "Config", NULL, MENU_ACTION_JUST_EXIT);
+     * 
+     * @param options cantidad de opciones que tendra el menu (numero entero > 0)
+     * @param title titulo del menu (c-string)
+     * @param on_exit funcion que se ejecuta al salir del menu. Colocar NULL para NO usar.
+     * @param action accion que realizara el menu cuando se ejecuta la funcion "menu_back()"
+     * 
+     */
+    Menu(uint8_t options, char* title, menu_callback_t on_exit, menu_action_t action);
+    ~Menu();
+
+    /**
+     * @brief Configurar una opcion de un menu
+     * - EJEMPLO:     menu_set_option(cfg_menu, 0, "Save data", config_save_data);
+     * 
+     * @param option_id numero de opcion que desea configurar (entre 0 y la cant. de opciones que tiene dicho menu)
+     * @param title titulo de la opcion (c-string)
+     * @param callback funcion que se ejecuta al seleccionar la opcion
+     */
+    void set_option(uint8_t option_id, char* title, menu_callback_t callback);
+
+    /**
+     * @brief Configurar escucha de eventos de control y funciones de visualizacion del menu
+     * - EJEMPLO:     menu_set_event_listener_display(keyboard_read, display_update);
+     * 
+     * @param ev_listener funcion que se ejecuta en TIEMPO REAL para escuchar nuevos eventos (ej: clicks de teclado)
+     *                      Colocar por ejemplo, un callback de lectura del joystick
+     * @param update_display funcion que se ejecuta cada vez que se deberia actualizar el display que muestra el menu.
+     *                      Dentro de esta funcion se deberia llamar menu_get_current_menu_data() para obtener
+     *                      los datos del menu actual y asi poder mostrarlos al usuario.
+     */
+    void set_event_listener_display(menu_callback_t ev_listener, menu_callback_t update_display);
+
+// (2) FUNCIONES DE MENU
+
+    /**
+     * @brief BLOQUEANTE - ejecuta y abre un menu. El programa queda atrapado dentro del mismo hasta que se cierre
+     */
+    void run();
+    /**
+     * @brief Cerrar el menu indicado
+     */
+    void force_close();
+    /**
+     * @brief Consultar si el menu esta activo
+     * 
+     * @retval true - ACTIVO
+     * @retval false - INACTIVO
+     */
+    bool is_available();
+
+    uint8_t getState() { return state; }
+    MenuData getData();
+
+    void go_up();
+    void go_down();
+    void go_select();
+    void go_back();
+
+private:
+    char **option_titles = NULL;   // titulos de cada opcion
+    menu_callback_t *option_callbacks = NULL; // callbacks de cada opcion
+    uint8_t current_option = 0; // opcion actual
+    uint8_t state = MENU_STATE_CLOSE;          // evento (estado) actual
+    char *title = NULL;            // Titulo del menu
+    uint8_t n_options = 0;      // cantidad de opciones
+    menu_action_t exit_action = MENU_ACTION_DO_NOTHING;  // accion de salida
+    void (*on_exit)(void) = NULL; // callback de salida
+};
+
+void set_animation_callback(menu_callback_t animation_f); // OPCIONAL. Loop en tiempo real
+
+// (3) FUNCIONES PARA NAVEGAR EN EL MENU (colocar en un callback asociado a un evento de control. ej: Teclado, Joystick)
+void menu_go_up(void);     // Ir a opcion superior
+void menu_go_down(void);   // Ir a opcion inferior
+void menu_go_select(void); // Seleccionar opcion
+void menu_go_back(void);   // Ir hacia atras
+
+
+// (4) FUNCIONES PARA MANIPULAR EL MENU EN EJECUCION ACTUAL
+bool menu_is_current_available(void);      // Retorna true si el menu actual esta activo
+void menu_force_close_current(void);       // fuerza el cierre del menu actual
+MenuData menu_get_current_data(void);   // Retorna una estructura con datos del menu actual (para mostrar en una UI, etc)
+
+#endif // _MENU_H_
